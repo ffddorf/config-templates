9c6162bfa76d9be60d1ce3f512a54188cd65fbdb
Marcus Weiner <marcus.weiner@gmail.com>
2025-04-12 19:24:50 +0200

    Apply templates using terraform

---
+++ config_templates.tf
@@ -0,0 +1,18 @@
+locals {
+  templates = {
+    for filename in fileset("${path.module}/templates", "*.j2") :
+    trimsuffix(filename, ".j2") => file("${path.module}/templates/${filename}")
+  }
+
+  template_assingments = {
+    for filename, template in local.templates :
+    filename => regex("{# ?assignment: ?platform=([^ #]+) ?#}", template)[0]
+  }
+}
+
+resource "netbox_config_template" "templates" {
+  for_each = local.templates
+
+  name          = try(regex("{# ?name: ?([^#]+?) ?#}", each.value)[0], each.key)
+  template_code = each.value
+}
